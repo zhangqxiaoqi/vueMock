4f1bc5aed7cc6a682db60e5e93f707e2c0a7abfa
zhangqi <1319419298@qq.com>
2017-05-10 16:25:14 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import axios from 'axios' 
+import Mock from 'mockjs'
+
+Vue.config.productionTip = false
+
+Vue.prototype.$ajax = axios
+Vue.prototype.$Mock = Mock
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
